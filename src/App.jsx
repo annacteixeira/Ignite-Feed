4efde2dcc8dc06ff2aabb6b38da2cce46cf9e017
Anna Chandra <annaftteixeira@gmail.com>
2025-04-11 12:47:41 -0300

    feat: sidebar component

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import { Header } from './components/Header'
 import Post from './Post';
 
 import './global.css';
+import styles from './components/App.module.css'
+import { Sidebar } from './components/Sidebar';
 
 export function App() { // componente App -> função que retorna HMTL
 
@@ -13,18 +15,23 @@ export function App() { // componente App -> função que retorna HMTL
     <>
       <div>
         {/* Trabalhando com propriedades em React */}
-        
+
         <Header />
 
-        <Post 
-          author="Anna Chandra" 
-          content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore cum iure doloribus et eveniet. Dolorum, impedit facilis?" 
-        />
+        <div className={styles.wrapper}>
+          <Sidebar />
+          <main>
+            <Post
+              author="Anna Chandra"
+              content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore cum iure doloribus et eveniet. Dolorum, impedit facilis?"
+            />
 
-        <Post 
-          author="Gabriel Victor" 
-          content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Distinctio suscipit, quaerat, nesciunt unde laboriosam cumque delectus ex expedita velit dolor maxime a ipsa! Sunt soluta id placeat, laudantium porro sint." 
-        />
+            <Post
+              author="Gabriel Victor"
+              content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Distinctio suscipit, quaerat, nesciunt unde laboriosam cumque delectus ex expedita velit dolor maxime a ipsa! Sunt soluta id placeat, laudantium porro sint."
+            />
+          </main>
+        </div>
       </div>
 
     </>
