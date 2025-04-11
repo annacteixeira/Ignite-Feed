a0bba430a34b542df322f42429efd758a0dd6017
Anna Chandra <annaftteixeira@gmail.com>
2025-04-11 12:00:40 -0300

    feat: created header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,12 @@
 import styles from './Header.module.css'
+import igniteLogo from '../assets/ignite-logo.svg';
+
 
 export function Header(){
     return(
         // para diferenciar as clases CSS das classes JS, as classes css devem ser chamadas usando className
         <header className={styles.header}>
-            <strong>Ignite Feed</strong>
+            <img src={igniteLogo} alt="Logotipo do Ignite" />
         </header>
         
     );
